4399284e2524806484385a7fcb5c7b77ad75122c
dhas8 <dhas8@DESKTOP-JBU9IT1>
2021-01-13 01:18:42 +0900

    웹 어플 만드는 데 뻘짓하다 시간만 날림..

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,8 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -7,26 +10,27 @@ import hello.core.member.Member;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.member.MemberRepository;
 
-public class OrderServiceImpl implements OrderService{
-	
+@Component
+public class OrderServiceImpl implements OrderService {
+
 	private final MemberRepository repo;
-	private final DiscountPolicy discountPolicy ;
-	
+	private final DiscountPolicy discountPolicy;
+
+	@Autowired
 	public OrderServiceImpl(MemberRepository repo, DiscountPolicy discountPolicy) {
 		this.repo = repo;
 		this.discountPolicy = discountPolicy;
 	}
 
-
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
-		
+
 		Member member = repo.findById(memberId);
 		int discountPrice = discountPolicy.dicount(member, itemPrice);
-		
+
 		return new Order(memberId, itemName, itemPrice, discountPrice);
 	}
-	
+
 	public MemberRepository getMemberRepository() {
 		return repo;
 	}
